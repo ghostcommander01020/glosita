fb1c1270e10579b8f5f70fc367bcbff7d3506109
ghostcommander0102 <192704293+ghostcommander01020@users.noreply.github.com>
2025-10-15 03:46:10 -0230

    update cors

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\ProxyController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
