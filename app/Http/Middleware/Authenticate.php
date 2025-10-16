efb417e96cf3444ecdce330fb828c933223c819d
ghostcommander0102 <192704293+ghostcommander01020@users.noreply.github.com>
2025-10-16 09:56:14 -0230

    update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,9 +4,26 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Support\Facades\Log;
 
 class Authenticate extends Middleware
 {
+    /**
+     * Handle an incoming request.
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        // Log auth header and resolved user (if any)
+        Log::info('auth:api middleware triggered', [
+            'auth_header' => $request->header('Authorization'),
+            'user_id'     => optional($request->user())->id,
+            'route'       => $request->path(),
+        ]);
+
+        return parent::handle($request, $next, ...$guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
